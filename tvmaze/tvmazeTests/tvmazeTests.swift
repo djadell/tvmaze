a22d6428e2218c166643ca267012a119e7820bd9
David <djadell.e@gmail.com>
2021-03-18 19:41:40 +0100

    Create view models tests

--- tvmaze/tvmazeTests/tvmazeTests.swift
+++ tvmaze/tvmazeTests/tvmazeTests.swift
@@ -17,11 +17,43 @@ class tvmazeTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testTvShowViewModel () {
+        let rating = Rating.init(average: 8.5)
+        let fakeTvShow = DBtvshow(id: 0,
+                                  url: "https://www.fakeurl.com/",
+                                  name: "Test tv show",
+                                  language: "Spanish",
+                                  rating: rating,
+                                  image: Image.init(medium: "ImageMedium", original: "ImageOriginal"),
+                                  summary: "This is the summary")
+        let tvShowViewModel = TvShowViewModel(tvShow: fakeTvShow)
+        // Test objects
+        XCTAssertNotNil(fakeTvShow)
+        XCTAssertNotNil(tvShowViewModel)
+        // Test values
+        XCTAssertEqual(fakeTvShow.name, tvShowViewModel.name)
+        XCTAssertEqual(fakeTvShow.language, tvShowViewModel.language)
+        XCTAssertEqual(fakeTvShow.image?.medium, tvShowViewModel.image)
+        XCTAssertEqual(fakeTvShow.rating?.average, tvShowViewModel.rating)
+        XCTAssertEqual(fakeTvShow.summary, tvShowViewModel.summary)
     }
+    
+    func testListViewModel() {
+        // Test Firts
+        ListVM.shared.fetchFirtsTvShows { (tvShowItmes) in
+            XCTAssert(!tvShowItmes.isEmpty, "The expected array of Tv shows are empty")
+        } failure: { (error) in
+            XCTAssertNil(error, "Service call failure")
+        }
+        // Test fetch next pages
+        ListVM.shared.fetchNextTvShows { (tvShowItemsNexPage) in
+            XCTAssert(!tvShowItemsNexPage.isEmpty, "The expected array of Tv shows are empty")
+        } failure: { (error) in
+            XCTAssertNil(error, "Service call failure")
+        }
+    }
+    
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
