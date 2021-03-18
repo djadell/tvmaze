0bef1ec58c6d1f2884381e60af063d640fecc2e7
David <djadell.e@gmail.com>
2021-03-18 20:21:53 +0100

    Service test and API

--- tvmaze/tvmazeTests/tvmazeTests.swift
+++ tvmaze/tvmazeTests/tvmazeTests.swift
@@ -18,6 +18,7 @@ class tvmazeTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
+    //MARK: - Tests
     func testTvShowViewModel () {
         let rating = Rating.init(average: 8.5)
         let fakeTvShow = DBtvshow(id: 0,
@@ -54,7 +55,58 @@ class tvmazeTests: XCTestCase {
         }
     }
     
-
+    func testServiceFetchData() {
+        // Test Firts
+        Service.shared.fetchFirtsTvShows { (tvShowItmes) in
+            XCTAssert(!tvShowItmes.isEmpty, "The expected array of Tv shows are empty")
+        } failure: { (error) in
+            XCTAssertNil(error, "Service call failure")
+        }
+        // Test fetch next pages
+        Service.shared.fetchNextTvShows { (tvShowItemsNexPage) in
+            XCTAssert(!tvShowItemsNexPage.isEmpty, "The expected array of Tv shows are empty")
+        } failure: { (error) in
+            XCTAssertNil(error, "Service call failure")
+        }
+    }
+    
+    func testServiceGetImages() {
+        let imageUrlString = "https://static.tvmaze.com/uploads/images/medium_portrait/1/4600.jpg"
+        Service.shared.getImageWithUrl(urlString: imageUrlString) { (image) in
+            XCTAssertNotNil(image, "The expected image should not be nil")
+        } failure: { (error) in
+            XCTAssertNil(error, "Download image failure")
+        }
+    }
+    
+    func testServerShowsAPI() throws {
+        let endOfTestExpectation = expectation(description: "RestServerAPI")
+        
+        let page = 1
+        let urlString = baseURL+showsIndexURL+"?page=\(page)"
+        guard let url = URL(string: urlString) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            XCTAssertNotNil(data, "The expected data should not be nil")
+            XCTAssertNil(error, "The expected error should be nil")
+            
+            guard let data = data else { return }
+            do {
+                XCTAssertNoThrow(try JSONDecoder().decode([DBtvshow].self, from: data), "")
+                let tvShows = try JSONDecoder().decode([DBtvshow].self, from: data)
+                XCTAssertNotNil(tvShows)
+                endOfTestExpectation.fulfill()
+            } catch let jsonError {
+                XCTAssertNil(jsonError, "Download image failure")
+                endOfTestExpectation.fulfill()
+            }
+        }.resume()
+        
+        waitForExpectations(timeout: 5) { (error) in
+            XCTAssertNil(error, "We got timeout")
+        }
+    }
+    
+    //MARK: - test Performance
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
